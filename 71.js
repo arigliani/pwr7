3ede29b0a94ef401d6c5a54a8918aa79bee09ac8
arigliani <gianni.arigliani@gmail.com>
2017-05-16 00:10:01 +0200

    non funzina, c' è problema nel js

---
+++ 71.js
@@ -0,0 +1,11 @@
+function maggiore() {  //non funziona e il problema e l'array
+    ￼var Vettore = new Array(5);
+    Vettore[0]=document.f.n1.value;
+    Vettore[1]=document.f.n2.value;
+    Vettore[2]=document.f.n3.value;
+    Vettore[3]=document.f.n4.value;
+    Vettore[4]=document.f.n5.value;
+   
+   window.alert();
+   
+}
